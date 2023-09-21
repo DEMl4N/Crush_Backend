297f48e3d65076579bff4bd88c5ae30c001e0e5a
Feedbaek <nicek789@naver.com>
2023-09-22 01:39:13 +0900

    Feat: CI/CD 완성

--- srcs/index.js
+++ srcs/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "../client/react", "index.html"));
