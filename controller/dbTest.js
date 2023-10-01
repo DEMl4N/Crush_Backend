9b07268305b9f3c791bc80a78b4d9523899f9366
feedbaek <nicek789@naver.com>
2023-10-01 22:01:51 +0900

    Fix: mongodb url 수정

--- controller/dbTest.js
+++ controller/dbTest.js
@@ -1,50 +1,52 @@
 const express = require('express');
+
+const logger = require('../config/logger');
+
 const router = express.Router();
 
 // mongoose 불러오기
-const { mongoose } = require('../database/mongoose');
+const mongoose = require('../database/mongoose');
 
 // 스키마와 모델 구성
 const testSchema = new mongoose.Schema({
-    testKey: String
+  testKey: String
 });
 
 const testModel = mongoose.model('dbTest', testSchema);
 
-
 router.get('/:testValue', (req, res) => {
-    testModel.findOne({
-        testKey: req.params.testValue
+  testModel
+    .findOne({
+      testKey: req.params.testValue
     })
     .then((document) => {
-        if (document) {
-            res.send("Found!");
-        }
-        else {
-            res.send("Not Found!");
-        }
+      if (document) {
+        res.send('Found!');
+      } else {
+        res.send('Not Found!');
+      }
     })
     .catch((error) => {
-        console.error(error);
-        res.status(400).send("Error!");
+      logger.error(error);
+      res.status(400).send('Error!');
     });
 });
 
 router.post('/:testValue', (req, res) => {
-    testModel.create({
-        testKey: req.params.testValue
+  testModel
+    .create({
+      testKey: req.params.testValue
     })
     .then((isSuccessful) => {
-        if (isSuccessful) {
-            res.send("Document Created!");
-        }
-        else {
-            res.send("Document Not Created!");
-        }
+      if (isSuccessful) {
+        res.send('Document Created!');
+      } else {
+        res.send('Document Not Created!');
+      }
     })
     .catch((error) => {
-        console.error(error);
-        res.status(400).send("Error!");
+      logger.error(error);
+      res.status(400).send('Error!');
     });
 });
 
--- database/mongoose.js
+++ database/mongoose.js
@@ -1,13 +1,27 @@
 // mongoose 설정
 const mongoose = require('mongoose');
 const mongoURI = require('../config/mongodb');
+const logger = require('../config/logger');
+require('dotenv').config();
 
-mongoose.connect(mongoURI.local)
-.then((response) => {
-  console.log("Successfully connected to MongoDB.");
-})
-.catch((error) => {
-  console.error(error);
-});
+if (process.env.NODE_ENV === 'production') {
+  mongoose
+    .connect(mongoURI.remote)
+    .then((response) => {
+      logger.info('Successfully connected to MongoDB.');
+    })
+    .catch((error) => {
+      logger.error(error);
+    });
+} else {
+  mongoose
+    .connect(mongoURI.local)
+    .then((response) => {
+      logger.info('Successfully connected to MongoDB.');
+    })
+    .catch((error) => {
+      logger.error(error);
+    });
+}
 
-module.exports.mongoose = mongoose;
+module.exports = mongoose;
