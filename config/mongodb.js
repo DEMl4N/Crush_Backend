ac07612b87cb50b92d9fcc98d3eec41ec69b403d
feedbaek <nicek789@naver.com>
2023-10-04 15:49:30 +0900

    Feat: mongoDB 계정 설정

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
+
 module.exports = {
   local: 'mongodb://127.0.0.1:27017/mylist',
-  remote: 'mongodb://34.64.96.158:27017/mylist'
+  remote: `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@34.64.96.158:27017/mylist`
 };
